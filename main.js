9563ac86dd129fe2859b5ae6828c694673bd16bb
1daniyarovaa <attokurovadiana9@gmail.com>
2023-01-07 16:52:48 +0300

    2dz

---
+++ main.js
@@ -0,0 +1,35 @@
+// //1
+
+// let i = 0;
+//  const printArrayValues = ()=> {
+//     i++;
+//     if(i >3){
+//   return "array"
+ 
+//     } 
+//     console.log(i , "array");
+//     printArrayValues()
+   
+//     // console.log(arr[i],'array');
+
+//  }
+//  printArrayValues()
+
+//2
+
+// let arr = [4,5,6]
+// arr.unshift('1,2,3')
+// arr.push(7,8,9,10)
+// console.log(arr);
+
+///3
+
+// let arr = [1,2,3,4,5]
+// console.log(arr[4]);
+
+////4
+ const str = "'loremipsumdolor','o'"
+ console.log(str.slice(4).match(/o/g));
+
+ 
+//  console.log("'loremipsumdolor,'o'".match(/o/g));
